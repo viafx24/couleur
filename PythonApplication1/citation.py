863176bc13022adb9a47e17b1c787f0e5233df47
viafx24 <42340158+viafx24@users.noreply.github.com>
2018-09-16 19:11:58 +0200

    Delete citation.py

--- PythonApplication1/citation.py
+++
@@ -1,9 +0,0 @@
-class citation:
-
-    def __init__(self,number,text):
-        self.number=number
-        self.text=text
-
-
-    def trainMode(self,date,req,timereq):
-        self.data={date:[req,timereq]}
